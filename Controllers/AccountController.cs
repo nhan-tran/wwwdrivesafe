8d20c5b9eaaf039434d1798f29ec1634f742dd93
Nhan Tran <nhan4545@gmail.com>
2015-01-30 00:29:24 -0800

    Hookup Azure DriveSafeDev DB

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -155,7 +155,7 @@ namespace wwwdrivesafe.Controllers
             if (ModelState.IsValid)
             {
                 var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
+                var result = UserManager.Create(user, model.Password);
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
