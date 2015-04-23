031dde70125da3e0f708e72a87c0a3f2032c0c14
Nhan Tran <nhan4545@gmail.com>
2015-04-22 19:44:41 -0700

    Dashboard page start

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using wwwdrivesafe.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+
+namespace wwwdrivesafe.Controllers
+{
+    public class DashboardController : Controller
+    {
+		private ApplicationDbContext db = new ApplicationDbContext();
+		private ApplicationUserManager _userManager;
+
+		public ApplicationUserManager UserManager
+		{
+			get
+			{
+				return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+			}
+			private set
+			{
+				_userManager = value;
+			}
+		}
+
+        // GET: Dashboard
+		[Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		[Authorize(Roles="SiteAdmin,BusinessAdmin,GroupAdmin")]
+		public ActionResult Manage()
+		{
+
+
+			return View();
+		}
+    }
+}
